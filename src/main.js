585cfec187d4c47cab4a6a7b74afdf4b33e62c60
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2022-11-04 11:15:16 +0100

    Change font

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { ViteSSG } from "vite-ssg"
 import "./style.css"
 import App from "./App.vue"
 import Houses from "./routes/Houses.vue"
+import "@fontsource/fredoka-one"
 
 const routes = [{ path: "/", component: Houses }]
 
